bac9bdf1c1a1cb48894b9a6538c2635f215c32ae
Ryan Ashby <rjapsu160@gmail.com>
2021-11-01 11:24:30 -0700

    add new placelist for a europe trip based on our created functions.
    Co-authored-by: Christina Welch <christinamawel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,21 @@ placesList.prototype.assignId = function() {
 placesList.prototype.addPlace = function(place) {
   place.id = this.assignId();
   this.places[place.id] = place;
-  console.log(place);
-  console.log(this.places);
+};
+
+placesList.prototype.findPlace = function(id) {
+  if (this.places[id] != undefined) {
+    return this.places[id];
+  }
+  return false;
+};
+
+placesList.prototype.deletePlace = function(id) {
+  if (this.places[id] === undefined) {
+    return false;
+  }
+  delete this.places[id];
+  return true;
 };
 
 // Business Logic for placesToSee ---------
@@ -24,3 +37,19 @@ function placesToSee (Location, Landmarks, TimeofYear, Notes, Food) {
   this.Notes = Notes;
   this.Food = Food;
 }
+
+let Paris = new placesToSee ("France", ["Eiffel Tower", "Louvre"], "Spring", "Lorem Ipsum", ["Baguette", "Croissant", "Cheese", "Wine"]);
+let Dublin = new placesToSee ("Ireland", ["Guinness Brewery", "Temple Bar"], "Summer", "Lorem Ipsum", ["Beer", "Haggis", "Corned Beef"]);
+let London = new placesToSee ("England", ["London Eye", "Tower of London", "Big Ben"], "Spring", "Lorem Ipsum", ["Fish & Chips", "English Breakfast"]);
+let Prague = new placesToSee ("Czech Republic", ["Bridge", "Churches"], "Winter", "Lorem Ipsum", ["Party Drugs", "Sandwiches"]);
+let Munich = new placesToSee ("Germany", ["Beer Tents", "Gardens"], "Spring", "Lorem Ipsum", ["Pretzel", "Schnitzel", "Cheese", "Sausage"]);
+
+let europePlaces = new placesList();
+
+europePlaces.addPlace(Paris);
+europePlaces.addPlace(Dublin);
+europePlaces.addPlace(London);
+europePlaces.addPlace(Prague);
+europePlaces.addPlace(Munich);
+
+console.log(europePlaces);
